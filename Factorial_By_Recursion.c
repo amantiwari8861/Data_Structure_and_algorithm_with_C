40085f64b2dfa70d0ad67f113605027378f11282
Aman <amantiwari8861@gmail.com>
2021-07-02 20:52:09 +0530

    initial

---
+++ Factorial_By_Recursion.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+//recursion : when a function call it itself it is called recursion
+int factorial(int num)
+{  
+    if (num>0)
+    {
+        return ( num*factorial(num-1) );
+    }
+    else
+    {
+        return 1;
+    }
+}
+int main()
+{
+    int num=0;
+    printf("enter the no. \n");
+    scanf("%d",&num);//5
+    int result=factorial(num);
+    printf("the result is %d \n",result);
+    return 0;
+}
