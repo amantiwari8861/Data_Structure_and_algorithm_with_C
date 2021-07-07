539500483ee28574f3ad937b5b97d73ab63822e9
Aman <amantiwari8861@gmail.com>
2021-07-07 15:22:05 +0530

    linear

---
+++ linear.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int num=0,element,flag=0;
+    printf("enter the size of array\n");
+    scanf("%d",&num);
+    int arr[num];
+    printf("enter the element in array \n");
+    for (int i = 0; i < num; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("enter the element to be searched \n");
+    scanf("%d",&element);
+
+    for (int i = 0; i < num; i++)
+    {
+        if (element==arr[i])
+        {
+            printf("found at position %d \n",i);
+            flag=1;
+        }
+    }
+    if (flag==0)
+    {
+        printf("element not found in array !\n");
+    }
+    return 0;
+}
