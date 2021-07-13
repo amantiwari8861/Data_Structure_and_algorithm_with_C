4d4b14d826d315c1d94d272f4e6b3eb46d372e2b
Aman <amantiwari8861@gmail.com>
2021-07-13 15:40:30 +0530

    sparse done!

---
+++ sparse.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+    int matr1[3][3],count=0;
+    printf("enter the matrix \n");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            scanf("%d",&matr1[i][j]);
+            if (matr1[i][j] != 0)
+            {
+                count++;
+            }
+        }
+    }
+    int sparse[3][count];
+    int k;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (matr1[i][j] != 0)
+            {
+                sparse[0][k]=i;
+                sparse[1][k]=j;
+                sparse[2][k]=matr1[i][j];
+                k++;
+            }
+        }
+        
+    }
+    printf("the matrix is :\n");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < count; j++)
+        {
+           printf(" %d \t",sparse[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
