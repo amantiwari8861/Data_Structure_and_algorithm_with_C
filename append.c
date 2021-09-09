03566f75d8f41bf3177c89e49ab27e753f551cc6
Aman <amantiwari8861@gmail.com>
2021-09-09 16:05:53 +0530

    append in linked list done

--- append.c
+++ append.c
@@ -1,78 +1,81 @@
-#include<stdio.h>
-#include<stdlib.h>
-struct node
-{
-	int data;
-	struct node *next; 
-}*start,*cn,*last,*head;
-void createNode()
-{
-    struct node *newNode;
-	newNode=(struct node*)malloc(sizeof(struct node));
-    if (newNode==NULL)
-    {
-        printf("unable to allocate the memory to newNode!");
-        exit(1);
-    }
-    printf("enter the data : ");
-    scanf("%d",&newNode->data);
-    fflush(stdin);
-    newNode->next=NULL;
-    start=newNode;
-    head=newNode;
-}
-void appendNode()
-{
-    struct node *append;
-    append=(struct node*)malloc(sizeof(struct node));
-    if (append==NULL)
-    {
-        printf("unable to allocate the memory to newNode!");
-        exit(1);
-    }
-    printf("enter the value : ");
-    scanf("%d",&append->data);
-    fflush(stdin);
-    append->next=NULL;
-    last=append;
-    last->next=append;
-}
-void printList()
-{
-    cn=start;
-    int count=0;
-    while (cn != NULL)
-    {
-        printf("the data in node %d is %d \n",++count,cn->data);
-        cn=cn->next;
-    }  
-}
-int main()
-{
-    char ch;
-    int choice;
-    do
-    {
-        printf("1. create first node.\n");
-        printf("2. print list.\n");
-        printf("3. append node \n");
-        printf("enter ur choice :");
-        scanf("%d",&choice);
-        fflush(stdin);
-        switch (choice)
-        {
-            case 1:createNode();
-                    break;
-            case 2: printList();
-                    break;
-            case 3: appendNode();
-                    break;
-            default:printf("there is no choice like this !");
-                    break;
-        }
-        printf("do u want to continue[y/Y] ? : ");
-        scanf("%c",&ch);
-        fflush(stdin);
-    } while (ch=='y' || ch=='Y');
-    return 0;
-}
+
+// wrong program 
+
+// #include<stdio.h>
+// #include<stdlib.h>
+// struct node
+// {
+// 	int data;
+// 	struct node *next; 
+// }*start,*cn,*last,*head;
+// void createNode()
+// {
+//     struct node *newNode;
+// 	newNode=(struct node*)malloc(sizeof(struct node));
+//     if (newNode==NULL)
+//     {
+//         printf("unable to allocate the memory to newNode!");
+//         exit(1);
+//     }
+//     printf("enter the data : ");
+//     scanf("%d",&newNode->data);
+//     fflush(stdin);
+//     newNode->next=NULL;
+//     start=newNode;
+//     head=newNode;
+// }
+// void appendNode()
+// {
+//     struct node *append;
+//     append=(struct node*)malloc(sizeof(struct node));
+//     if (append==NULL)
+//     {
+//         printf("unable to allocate the memory to newNode!");
+//         exit(1);
+//     }
+//     printf("enter the value : ");
+//     scanf("%d",&append->data);
+//     fflush(stdin);
+//     append->next=NULL;
+//     last=append;
+//     last->next=append;
+// }
+// void printList()
+// {
+//     cn=start;
+//     int count=0;
+//     while (cn != NULL)
+//     {
+//         printf("the data in node %d is %d \n",++count,cn->data);
+//         cn=cn->next;
+//     }  
+// }
+// int main()
+// {
+//     char ch;
+//     int choice;
+//     do
+//     {
+//         printf("1. create first node.\n");
+//         printf("2. print list.\n");
+//         printf("3. append node \n");
+//         printf("enter ur choice :");
+//         scanf("%d",&choice);
+//         fflush(stdin);
+//         switch (choice)
+//         {
+//             case 1:createNode();
+//                     break;
+//             case 2: printList();
+//                     break;
+//             case 3: appendNode();
+//                     break;
+//             default:printf("there is no choice like this !");
+//                     break;
+//         }
+//         printf("do u want to continue[y/Y] ? : ");
+//         scanf("%c",&ch);
+//         fflush(stdin);
+//     } while (ch=='y' || ch=='Y');
+//     return 0;
+// }
---
+++ appendInLinkedList.c
@@ -0,0 +1,88 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *link;
+}*cn,*last;
+
+struct Node *start=NULL;
+struct Node* createNode()
+{
+    printf("Creating a Node \n");
+    struct Node *node1;
+    node1=(struct Node*)malloc(sizeof(struct Node*));
+    if (node1==NULL)
+    {
+        printf("unable to allocate the memory to newNode!");
+        exit(1);
+    }
+    printf("enter the data :");
+    scanf("%d",&node1->data);
+    fflush(stdin);
+    node1->link=NULL;
+    return node1;
+}
+void appendNode()
+{
+    struct Node *newNode;
+    newNode=createNode();
+    if (start==NULL)
+    {
+        printf("creating new List \n");
+        start=newNode;
+        last=newNode;
+        newNode->link=NULL;
+    }
+    else
+    {
+        printf("adding in old list\n");
+        last=start;
+		while(last->link!=NULL)
+		{
+			last=last->link;
+		}
+		last->link=newNode;
+		last=newNode;
+		newNode->link=NULL;
+    }
+}
+void printList()
+{
+    cn=start;
+    int count=0;
+    while (cn != NULL)
+    {
+        printf(" the data at %d node is %d \n",++count,cn->data);
+        cn=cn->link;
+    } 
+}
+int main()
+{
+    char ch;
+    int choice;
+    do
+    {
+        printf("1. create first node.\n");
+        printf("2. print list.\n");
+        printf("3. append node \n");
+        printf("enter ur choice :");
+        scanf("%d",&choice);
+        fflush(stdin);
+        switch (choice)
+        {
+            case 1: createNode();
+                    break;
+            case 2: printList();
+                    break;
+            case 3: appendNode();
+                    break;
+            default:printf("there is no choice like this !");
+                    break;
+        }
+        printf("do u want to continue[y/Y] ? : ");
+        scanf("%c",&ch);
+        fflush(stdin);
+    } while (ch=='y' || ch=='Y');
+    return 0;
+}
