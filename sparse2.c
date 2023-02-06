c92894498a7c8d758a6c4ba694789448cddd926c
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-02-06 20:09:06 +0530

    sparse matrix

--- sparse2.c
+++ sparse2.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #define SUB_MAX 10
-#define MAX 20
+#define MAX 32
 
-void read_matrix(int a[SUB_MAX][SUB_MAX], int row, int column);
-void print_sparse(int b[MAX][3]);
-void create_sparse(int a[SUB_MAX][SUB_MAX], int row, int column, int b[MAX][3]);
+void read_matrix(int a[][SUB_MAX], int row, int column);
+void print_sparse(int b[][3]);
+void create_sparse(int a[][SUB_MAX], int row, int column, int b[][3]);
+void recreateMatrix(int b[][3],int rearr[][SUB_MAX]);
 
 int main()
 {
-    int a[SUB_MAX][SUB_MAX], b[MAX][3], row, column;
+    // if the quantity of non-zeroes is less than 32% then use sparse matrix 
+    // 4*100 = 400 bytes
+    // 33*4*3 = 396 bytes
+    int a[SUB_MAX][SUB_MAX], b[MAX][3], row, column,c[SUB_MAX][SUB_MAX]={0};
     printf("\nEnter the size of matrix (rows, columns): \n");
     scanf("%d%d", &row, &column);
 
     read_matrix(a, row, column);
     create_sparse(a, row, column, b);
     print_sparse(b);
+    recreateMatrix(b,c);
     return 0;
 }
 
@@ -26,7 +31,6 @@ void read_matrix(int a[SUB_MAX][SUB_MAX], int row, int column)
     {
         for (j = 0; j < column; j++)
         {
-            // printf("[%d][%d]: ", i, j);
             scanf("%d", &a[i][j]);
         }
     }
@@ -56,16 +60,55 @@ void create_sparse(int a[SUB_MAX][SUB_MAX], int row, int column, int b[MAX][3])
 
 void print_sparse(int b[MAX][3])
 {
-    int i, column;
-    column = b[0][2];
+    int i,total = b[0][2];
     printf("\nSparse form - list of 3 triples\n\n");
-    printf("row    column  values \n");
-    printf("---   -----    ----\n");
-    for (i = 0; i <= column; i++)
+    printf(" row   column    value \n");
+    printf("----   ------    -----\n");
+    for (i = 0; i <= total; i++)
     {
         if(i==1)
-        printf("---   -----    ----\n");
-        printf("%d\t%d\t%d\n", b[i][0], b[i][1], b[i][2]);
+        {     
+            printf("----   ------    -----\n");   
+        }
+        // printf("%d\t%d\t%d\n", b[i][0], b[i][1], b[i][2]);
+        printf("%4d%9d%8d\n", b[i][0], b[i][1], b[i][2]);
     }
 }
-//h.w deletion insertion updation in array
+void recreateMatrix(int b[][3],int rearr[][SUB_MAX])
+{
+    int row=b[0][0],col=b[0][1],k=1;
+    // for (int i = 0; i < row; i++)
+    // {
+    //     for (int j = 0; j < col; j++)
+    //     {
+    //         if (b[k][0]==i && b[k][1]==j)
+    //         {
+    //             rearr[i][j]=b[k][2];
+    //             k++;
+    //         }
+    //         else
+    //         {
+    //             rearr[i][j]=0;
+    //         }
+    //     }
+    // }
+    for (int i = 1; i <= b[0][2]; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            rearr[b[i][0]][b[i][1]]=b[i][2];
+        }
+    }
+    
+    printf("the Recovered Array is :\n");
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            printf(" %d \t",rearr[i][j]);
+        }
+        printf("\n");
+    }
+    
+    
+}
