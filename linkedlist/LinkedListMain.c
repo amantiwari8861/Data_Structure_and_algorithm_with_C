ca4a1d86ec761d8341d7984491b2c212daf9a01c
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-02-13 19:39:00 +0530

    insert at position

--- linkedlist/LinkedListMain.c
+++ linkedlist/LinkedListMain.c
@@ -1,8 +1,19 @@
+#define RESET "\x1b[0m"
+#define RED "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_BGCOLOR_RED   "\x1b[41m"
+#define ANSI_BGCOLOR_GREEN   "\x1b[42m"
+
 #include<stdio.h>
 #include<stdlib.h>
-void addNode();
+void addLast();
+void addAtPos();
 void printList();
-
+int count();
 struct Node
 {
     int data;
@@ -18,6 +29,7 @@ int main()
         printf("\n-----------------Linked List Program------------------------\n");
         printf("1.Add Node \n");
         printf("2.Print List \n");
+        printf("3.Add at Position \n");
         printf("5.Exit \n");
 
         printf("enter choice :");
@@ -26,10 +38,13 @@ int main()
         switch (choice)
         {
         case 1:
-            addNode();
+            addLast();
             break;
         case 2:
             printList();
+            break;
+        case 3:
+            addAtPos();
             break;
         case 5:
             exit(0);
@@ -45,7 +60,7 @@ int main()
     
     return 0;
 }
-void addNode()
+void addLast()
 {
     struct Node *newnode;
     newnode=(struct Node*)malloc(sizeof(struct Node));
@@ -75,4 +90,53 @@ void printList()
         cn=cn->next;
     }
     printf("\b]\n");
+}
+int count()
+{
+    cn=head;
+    int num=0;
+    while (cn!=NULL)
+    {
+        num++;
+        cn=cn->next;
+    }
+    printf("total element is %d \n",num);
+    return num;
+}
+void addAtPos()
+{
+    count();
+    int pos;
+    printf("enter position to insert :");
+    scanf("%d",&pos);
+
+    if (pos==1)
+    {
+        /*insert at stating pos*/
+        struct Node* newnode=(struct Node*)malloc(sizeof(struct Node));
+        printf("enter the data:");
+        scanf("%d",&newnode->data);
+        newnode->next=head;
+        head=newnode;
+    }
+    else if(pos>1)
+    {
+        struct Node* newnode=(struct Node*)malloc(sizeof(struct Node));
+        printf("enter the data:");
+        scanf("%d",&newnode->data);
+        cn=head;
+        for(int i=2;cn != NULL;i++)
+        {            
+            if(pos==i)
+            {
+                newnode->next=cn->next;
+                cn->next=newnode;
+            }
+            cn=cn->next;
+        }
+    }
+    else
+    {
+        printf(RED"pls enter a valid position!!"RESET);
+    }
 }
