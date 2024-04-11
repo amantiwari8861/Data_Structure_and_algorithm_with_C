445e43ba98f74d2aa8d07494467a08a568381493
Aman Tiwari <amantiwari8861@gmail.com>
2024-04-11 12:21:17 +0530

    stack using array

--- Arrays/stack/stack.c
+++ Arrays/stack/stack.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
+#include <stdlib.h>
+#include<conio.h>
 #define MAX 10
 
 int top=-1;
-void add(int stack[],int num)
+void push(int stack[],int num)
 {
     if (top==MAX-1)
     {
@@ -10,6 +12,7 @@ void add(int stack[],int num)
         return;
     }
     stack[++top]=num;
+    printf("pushed into stack\n");
 }
 void printStack(int stack[])
 {
@@ -20,16 +23,66 @@ void printStack(int stack[])
     }
     printf("\n");   
 }
-
+void pop(int stack[])
+{
+    stack[top--]=-1;
+    printf("popped an element!\n");
+}
+void peek(int stack[])
+{
+    printf("at top the element is %d \n",stack[top]);
+}
 int main()
 {
-    int stack[MAX];
+    int stack[MAX],choice,temp;
 
-    add(stack,70);
-    add(stack,80);
-    add(stack,10);
+    // push(stack,70);
+    // push(stack,80);
+    // push(stack,10);
+    // pop(stack);
+    // peek(stack);
+    // printStack(stack);
 
-    printStack(stack);
+    do
+    {
+        system("cls");
+        printf("1.push element into stack\n");
+        printf("2.pop element from stack\n");
+        printf("3.peek into stack\n");
+        printf("4.print stack\n");
+        printf("0.exit\n");
+        printf("enter ur choice:");
+        scanf("%d",&choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("enter a value to push:");
+            scanf("%d",&temp);
+            push(stack,temp);
+            break;
+        case 2:
+            pop(stack);
+            break;
+        case 3:
+            peek(stack);
+            break;
+        case 4:
+            printStack(stack);
+            break;
+        case 0:
+            printf("Thank You!!");
+            exit(0);
+            break;
+        
+        default:printf("invalid choice!!");
+            break;
+        }
+        printf("press any key to continue....");
+        getch();
+    } while (choice!=0);
+    
+
 
     return 0;
 }
